0dad8adf749f27ba5d5399b30ba1e4c033f80686
JavaScriptErika <erikajritter@gmail.com>
2022-02-23 12:28:54 -0500

    add id to treeCardData, add FlatList to SeeFavorites

--- src/Grass/SeeFavorites.tsx
+++ src/Grass/SeeFavorites.tsx
@@ -1,17 +1,66 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, ScrollView, View } from 'react-native';
+import { StyleSheet, Text, ScrollView, View, FlatList } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const SeeFavorites = ({route}) => {
 
     const {treeCardFacts} = route.params
-    
-    const favoritedFacts = treeCardFacts.filter(fact => fact.favorited === true)
-    console.log(favoritedFacts)
-    return (
-        <View>
 
-        </View>
-    )
+    const favoritedFacts = treeCardFacts.filter(fact => fact.favorited === true)
+    console.log(favoritedFacts.length)
+
+    if(favoritedFacts.length > 0) {
+        return (
+            <View style={styles.container}>
+                <FlatList 
+                    data={favoritedFacts}
+                    renderItem={({item: {quote, id}}) => (
+                        <View style={styles.card}> 
+                            <Text style={styles.cardText}>
+                                {quote}
+                            </Text>
+                        </View>
+                    )}
+                />
+            </View>
+        )
+    } else {
+        return (
+            <View style={styles.noShowContainer}>
+               <Ionicons 
+                    name="md-leaf"
+                    size={32} 
+                    color="#3a9a63" 
+                />
+               <Text>No current favorites to show</Text>
+            </View>
+        )
+    }
+
 }
 
 export default SeeFavorites
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fefcfd',
+        flex: 1,
+    },
+    card: {
+        backgroundColor: '#e5f1ea',
+        padding: 12,
+        marginVertical: 10,
+        borderRadius: 20,
+        width: '90%',
+        alignSelf: 'center'
+    },
+    cardText: {
+        color: '#135658',
+        fontSize: 25,
+    },
+    noShowContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    }
+})
